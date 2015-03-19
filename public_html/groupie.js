171e36bf7ec81272e3ae58ef6b2fa61d4f46f420
revanth <revanth.reddy@gmail.com>
2015-03-18 22:55:23 -0500

    local artist tracklist

--- public_html/groupie.js
+++ public_html/groupie.js
@@ -3,17 +3,57 @@ var app = express();
 var server = require('http').createServer(app);
 
 var http = require('http');
-var LastFmNode = require('lastfm').LastFmNode;
+var config = {rdio_api_key: 'fzcukawmjkemz8bvqf522m37', rdio_api_shared: '9hkN7FEJPn'};
+var rdio = require('rdio')(config);
+var OAuth = require('oauth-1.0a');
+var request = require('request');
+var async = require('async');
 
-var geocoder = require('geocoder');
-geocoder.reverseGeocode( 33.7489, -84.3789, function ( err, data ) {
-  console.log(data);
+var oauth = OAuth({
+    consumer: {
+        public: 'fzcukawmjkemz8bvqf522m37',
+        secret: '9hkN7FEJPn'
+    },
+    signature_method: 'HMAC-SHA1'
 });
 
-var lastfm = new LastFmNode({
-    api_key: '7bf99143ef8da6a02a70aec40475e258', // sign-up for a key at http://www.last.fm/api
-    secret: '68fe86acc21a210367f429e2b69bc0d2'
-});
+var token = {
+    public: 'esgj5rtwcx7f43he6j6r34grf5ztgd5ka8nw39rbvaxtzcyyytsk83ne8fwp8yju',
+    secret: 't6wrSXqTtZS5'
+};
+
+//var request_data = {
+//    url: 'http://api.rdio.com/1/',
+//    method: 'POST',
+//    data: {
+//        artist: "r139688",
+//        extras: "iframeUrl",
+//        count: 1,
+//        method: "getTracksForArtist"
+//    }
+//};
+//request({
+//    url: request_data.url,
+//    method: request_data.method,
+//    form: oauth.authorize(request_data, token)
+//}, function (error, response, body) {
+//    console.log(body);
+//});
+
+
+//var geocoder = require('geocoder');
+//geocoder.reverseGeocode( 33.7489, -84.3789, function ( err, data ) {
+//  console.log(data);
+//});
+//rdio.api('fzcukawmjkemz8bvqf522m37', '9hkN7FEJPn', {
+//                method: 'search',
+//                query : 'spoon',
+//                type: 'artist',
+//                count: 1
+//            }, function(err, data){
+//                console.log(data);
+//            });
+
 
 server.listen(3000);
 app.use(express.bodyParser());
@@ -27,33 +67,98 @@ app.get('/', function (req, res) {
     res.send("hello");
 });
 
-app.get('/artists', function (req, res) {
+app.get('/artists/:city/playlist', function (req, res) {
 
-    var city = req.query.param;
+    var city = req.param('city');
     if (!city)
         city = "austin";
 
     var options = {
         host: 'developer.echonest.com',
-        path: '/api/v4/artist/search?api_key=00ZKZFHPDBMHGHB40&format=json&results=10&artist_location=austin',
+        path: '/api/v4/artist/search?api_key=00ZKZFHPDBMHGHB40&format=json&results=10&artist_location='+city,
         method: 'GET'
     };
-    var url = "http://developer.echonest.com/api/v4/artist/search?api_key=00ZKZFHPDBMHGHB40&format=json&results=10&artist_location=austin";
-    var geoArtists = http.request (options ,function(response) {
+    
+    var geoArtists = http.request(options, function (response) {
         var str = '';
         response.on('data', function (chunk) {
             str += chunk;
         });
 
         response.on('end', function () {
-            str=JSON.parse(str);
-//            var artists = str.toptracks;
-//            var returnObject = [];
-//            console.log(artists.track.length);
-//            for(var i = 0 ; i < artists.track.length ; i++){
-//                returnObject.push(artists.track[i].artist);
-//            }
-            return res.status(200).send(str.response.artists);
+            str = JSON.parse(str);
+            var geoTrackList = [];
+            async.each(str.response.artists, function (artist, callback) {
+
+                // Perform operation on file here.
+                console.log('artist: ' + artist.name);
+                setTimeout(function(){ 
+                
+                
+                
+                var request_data = {
+                    url: 'http://api.rdio.com/1/',
+                    method: 'POST',
+                    data: {
+                        query: artist.name,
+                        types: "artist",
+                        count:1,
+                        method:"search"
+                    }
+                };
+                request({
+                    url: request_data.url,
+                    method: request_data.method,
+                    form: oauth.authorize(request_data, token)
+                }, function (error, response, body) {  
+                    if(error)
+                        callback(error);
+                        body = JSON.parse(body);        //get tp song of an artist
+                                var artist_song= {
+                                url: 'http://api.rdio.com/1/',
+                                method: 'POST',
+                                data: {
+                                    artist: body.result.results[0].key,
+                                    extras: "iframeUrl",
+                                    count: 1,
+                                    method: "getTracksForArtist"
+                                    }
+                                };
+                                request({
+                                    url: artist_song.url,
+                                    method: artist_song.method,
+                                    form: oauth.authorize(artist_song, token)
+                                }, function (error, response, track) {
+                                    geoTrackList.push(JSON.parse(track));
+                                    callback();
+                                });
+                    
+                    
+                    
+                });
+                
+                
+                
+                
+                }, 200);
+                
+
+            }, function (err) {
+                // if any of the file processing produced an error, err would equal that error
+                if (err) {
+                    // One of the iterations produced an error.
+                    // All processing will now stop.
+                    console.log('A file failed to process');
+                    return res.status(400).send("Artist processing failed");
+                } else {
+                    console.log('All artists have been processed successfully');
+                    return res.status(200).send(geoTrackList);
+                }
+            });
+
+
+
+//            return res.status(200).send(str.response.artists);
         });
     });
 
