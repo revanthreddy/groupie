e5ae81ca261eddeab2999f438827a21da41a2e61
revanth <revanth.reddy@gmail.com>
2015-03-20 10:52:02 -0500

    edward sharpe

--- public_html/groupie.js
+++ public_html/groupie.js
@@ -58,20 +58,20 @@ app.get('/', function (req, res) {
 });
 
 app.get('/artists/:city/playlist', function (req, res) {
-    
+
     var city = req.param('city');
     var startIndex = req.query.start;
-    if(!startIndex)
+    if (!startIndex)
         startIndex = 0;
     if (!city)
         city = "austin";
-    var echoNestPath = encodeURI('/api/v4/artist/search?api_key=00ZKZFHPDBMHGHB40&format=json&results=5&start='+startIndex+'&artist_location='+city);
+    var echoNestPath = encodeURI('/api/v4/artist/search?api_key=00ZKZFHPDBMHGHB40&format=json&results=5&start=' + startIndex + '&artist_location=' + city);
     var options = {
         host: 'developer.echonest.com',
         path: echoNestPath,
         method: 'GET'
     };
-    
+
     var geoArtists = http.request(options, function (response) {
         var str = '';
         response.on('data', function (chunk) {
@@ -81,37 +81,35 @@ app.get('/artists/:city/playlist', function (req, res) {
         response.on('end', function () {
             str = JSON.parse(str);
             var geoTrackList = [];
+            var counter = 0;
             async.each(str.response.artists, function (artist, callback) {
-
+                counter++;
                 // Perform operation on file here.
                 console.log('artist: ' + artist.name);
-                
-                
-                
-                
+
                 var request_data = {
                     url: 'http://api.rdio.com/1/',
                     method: 'POST',
                     data: {
                         query: artist.name,
                         types: "artist",
-                        count:1,
-                        method:"search"
+                        count: 1,
+                        method: "search"
                     }
                 };
-                
-                setTimeout(function(){
-                request({
-                    url: request_data.url,
-                    method: request_data.method,
-                    form: oauth.authorize(request_data, token)
-                }, function (error, response, body) {  
-                    if(error)
-                        callback(error);
-                        body = JSON.parse(body);    
+
+                setTimeout(function () {
+                    request({
+                        url: request_data.url,
+                        method: request_data.method,
+                        form: oauth.authorize(request_data, token)
+                    }, function (error, response, body) {
+                        if (error)
+                            callback(error);
+                        body = JSON.parse(body);
                         //get tp song of an artist
-                        setTimeout(function(){
-                                var artist_song= {
+                        setTimeout(function () {
+                            var artist_song = {
                                 url: 'http://api.rdio.com/1/',
                                 method: 'POST',
                                 data: {
@@ -119,35 +117,40 @@ app.get('/artists/:city/playlist', function (req, res) {
                                     extras: "iframeUrl",
                                     count: 1,
                                     method: "getTracksForArtist"
-                                    }
-                                };
-                                request({
-                                    url: artist_song.url,
-                                    method: artist_song.method,
-                                    form: oauth.authorize(artist_song, token)
-                                }, function (error, response, track) {
-                                    if(error)
-                                        callback(error);
-                                   try {
-                                       track = JSON.parse(track);
-                                       if(track.result[0].key !== 't12204728')
-                                            geoTrackList.push(track);
-                                        else
-                                            console.log('ignoring t12204728');
-                                    callback();
-                                    }
-                                    catch(err) {
-                                        callback(error);
-                                    }
+                                }
+                            };
+                            request({
+                                url: artist_song.url,
+                                method: artist_song.method,
+                                form: oauth.authorize(artist_song, token)
+                            }, function (error, response, track) {
+                                if (error)
+                                    callback(error);
+                                try {
+                                    track = JSON.parse(track);
                                     
-                                });
-                    
-                        } , 500);
-                    
-                });
-            } , 500);
-                
-                
+                                    if (track.result[0].key !== 't12204728') {
+                                        track.result[0].ticketInfo = {"showdate": "", showtime: ""};
+                                        geoTrackList.push(track);
+                                    }
+                                    else
+                                        console.log('ignoring t12204728');
+                                       
+                                    callback();
+                                    
+                                }
+                                catch (err) {
+                                    callback(error);
+                                }
+
+                            });
+
+                        }, 500);
+
+                    });
+                }, 500);
+
+
 
             }, function (err) {
                 // if any of the file processing produced an error, err would equal that error
@@ -158,6 +161,9 @@ app.get('/artists/:city/playlist', function (req, res) {
                     return res.status(400).send("Artist processing failed");
                 } else {
                     console.log('All artists have been processed successfully');
+                    if(city === "west palm beach"){
+                        geoTrackList[1] = getEdwardSharpeInfo();
+                    }
                     return res.status(200).send(geoTrackList);
                 }
             });
@@ -174,5 +180,53 @@ app.get('/artists/:city/playlist', function (req, res) {
 
 });
 
+
+
+function getEdwardSharpeInfo() {
+    var track = {
+        "status": "ok",
+        "result": [
+            {
+                "key": "t3984167",
+                "baseIcon": "album/8/0/9/000000000004e908/2/square-200.jpg",
+                "canDownloadAlbumOnly": false,
+                "radio": {
+                    "type": "sr",
+                    "key": "sr3984167"
+                },
+                "artistUrl": "/artist/Edward_Sharpe__The_Magnetic_Zeros/",
+                "duration": 303,
+                "album": "Edward Sharpe & The Magnetic Zeros",
+                "isClean": false,
+                "albumUrl": "/artist/Edward_Sharpe__The_Magnetic_Zeros/album/Edward_Sharpe__The_Magnetic_Zeros_2/",
+                "shortUrl": "http://rd.io/x/Qit-lGQ/",
+                "albumArtist": "Edward Sharpe & The Magnetic Zeros",
+                "canStream": true,
+                "embedUrl": "https://rd.io/e/Qit-lGQ/",
+                "type": "t",
+                "gridIcon": "http://rdiodynimages1-a.akamaihd.net/?l=a321800-2%3Aboxblur%2810%25%2C10%25%29%3Ba321800-2%3Aprimary%280.65%29%3B%240%3Aoverlay%28%241%29%3Ba321800-2%3Apad%2850%25%29%3B%242%3Aoverlay%28%243%29",
+                "price": null,
+                "trackNum": 6,
+                "albumArtistKey": "r396228",
+                "radioKey": "sr3984167",
+                "icon": "http://img02.cdn2-rdio.com/album/8/0/9/000000000004e908/2/square-200.jpg",
+                "canSample": true,
+                "name": "Home",
+                "isExplicit": false,
+                "artist": "Edward Sharpe & The Magnetic Zeros",
+                "url": "/artist/Edward_Sharpe__The_Magnetic_Zeros/album/Edward_Sharpe__The_Magnetic_Zeros_2/track/Home/",
+                "icon400": "http://img00.cdn2-rdio.com/album/8/0/9/000000000004e908/2/square-400.jpg",
+                "artistKey": "r396228",
+                "canDownload": false,
+                "length": 1,
+                "canTether": true,
+                "albumKey": "a321800",
+                "ticketInfo": {"showdate": "May 2nd 2015", showvenue: "Sunfest"}
+            }
+        ]
+    }
+
+    return track;
+}
 
 
